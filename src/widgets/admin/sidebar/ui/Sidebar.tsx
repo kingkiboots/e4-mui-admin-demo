a22cf8cdb9a6aec04ac8bc9565eb9dd61918a86a
kingkiboots <tuffhr4@gmail.com>
2025-10-05 01:37:46 +0900

    Feat: sidebar 레이아웃 mui로 변경

--- src/widgets/admin/sidebar/ui/Sidebar.tsx
+++ src/widgets/admin/sidebar/ui/Sidebar.tsx
@@ -1,7 +1,39 @@
 import { memo } from "react";
+import SidebarHeader from "./SidebarHeader";
+import SidebarNav from "./SidebarNav";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
+type ExtraProps = {
+  component: React.ElementType;
+};
+
+const StyledSidebar = styled(Box, {
+  name: "Sidebar",
+  label: "page-sidebar",
+})<ExtraProps>(({ theme }) => ({
+  position: "relative",
+  flex: "1 0 auto",
+  width: "27rem",
+  maxWidth: "27rem",
+  flexDirection: "column",
+  display: "flex",
+  zIndex: 1002,
+  willChange: "left, right",
+
+  backgroundColor: theme.palette.sidebar?.background,
+  // 그라디언트 배경
+  backgroundImage:
+    "linear-gradient(270deg, rgba(51, 148, 225, 0.18), transparent)",
+}));
 
 const Sidebar = memo(() => {
-  return <div>Sidebar</div>;
+  return (
+    <StyledSidebar component="aside">
+      <SidebarHeader />
+      <SidebarNav />
+    </StyledSidebar>
+  );
 });
 
 Sidebar.displayName = "Sidebar";
