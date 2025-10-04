f98d7f4abd200b05cd61b449fb9857d8f87d764f
kingkiboots <tuffhr4@gmail.com>
2025-10-05 01:29:01 +0900

    Feat: 로고이미지 공통 컴포넌트로 추가
    여러 군데에서 많이 쓰일 것 같아서 이미지 태그만 딸랑 해놓음

---
+++ src/shared/ui/LogoUI.tsx
@@ -0,0 +1,22 @@
+import { memo } from "react";
+import logoImg from "../asset/img/owlog_logo.png";
+import { Image } from "./ImgUI";
+
+interface LogoProps
+  extends Omit<
+    React.ComponentProps<typeof Image>,
+    "src" | "alt" | "aria-roledescription"
+  > {}
+
+export const Logo = memo<LogoProps>((props) => {
+  return (
+    <Image
+      src={logoImg}
+      alt="마이데이터"
+      aria-roledescription="logo"
+      {...props}
+    />
+  );
+});
+
+Logo.displayName = "Logo";
