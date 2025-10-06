fef9ae91947049dc73aaaa9e354662942e84db06
kingkiboots <tuffhr4@gmail.com>
2025-10-07 01:37:55 +0900

    Feat: themeConfig에 spacing system과 sidebar에서 쓰일 컬러 팔레트값 추가

--- src/shared/config/themeConfig.ts
+++ src/shared/config/themeConfig.ts
@@ -6,23 +6,21 @@ declare module "@mui/material/styles" {
     sidebar: {
       background: string;
       text: string;
+      menu: string;
+      active: string;
+      hover: string;
     };
   }
-  interface PaletteOptions {
-    sidebar?: {
-      background?: string;
-      text?: string;
-      menu?: string;
-      active?: string;
-    };
-  }
+  interface PaletteOptions extends Partial<Palette> {}
 }
 
 export const themeConfig = createTheme({
   typography: {
     fontFamily: '"Pretendard", "Noto Sans KR", sans-serif',
-    htmlFontSize: 10, // 62.5%
+    htmlFontSize: 10, // 1rem = 10px (62.5%)
   },
+  spacing: (factor: number) =>
+    `${[4, 8, 16, 24, 32, 40, 48, 56, 64][factor] / 10}rem`,
   palette: {
     primary: {
       main: "#886ab5",
@@ -41,7 +39,7 @@ export const themeConfig = createTheme({
       text: "#ffffff",
       menu: "#b5b5b5",
       active: "#ff5a00",
+      hover: "rgba(0, 0, 0, 0.1)",
     },
   },
-  spacing: 8,
 });
