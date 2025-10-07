e842400d94fcf6d4d79132b407dabc2a90bda294
kingkiboots <tuffhr4@gmail.com>
2025-10-08 02:03:33 +0900

    Fix: PageContentWrapper flexDirection: column 이길래 stack 으로 변경

--- src/shared/layout/PageContentWrapper.tsx
+++ src/shared/layout/PageContentWrapper.tsx
@@ -1,15 +1,16 @@
 import { styled } from "@mui/material/styles";
-import Box, { type BoxProps } from "@mui/material/Box";
+import Stack, { type StackProps } from "@mui/material/Stack";
 import { UI_DRAWER_WIDTH } from "../const";
 
-interface PageContentWrapperProps extends BoxProps {
+interface PageContentWrapperProps extends StackProps {
   isDrawerOpen: boolean;
 }
-export const PageContentWrapper = styled(Box, {
+export const PageContentWrapper = styled(Stack, {
   label: "page-content-wrapper",
   shouldForwardProp: (prop) => prop !== "isDrawerOpen",
 })<PageContentWrapperProps>(({ theme }) => ({
   flex: 1,
+  backgroundColor: theme.palette.background.paper,
   transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
