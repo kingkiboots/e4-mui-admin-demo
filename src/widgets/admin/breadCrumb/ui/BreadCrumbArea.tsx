dea76df64b91ca3a5c2b25c224fda078bef594f1
kingkiboots <tuffhr4@gmail.com>
2025-10-08 23:04:15 +0900

    Feat: 브레드크럼브 스타일링

--- src/widgets/admin/breadCrumb/ui/BreadCrumbArea.tsx
+++ src/widgets/admin/breadCrumb/ui/BreadCrumbArea.tsx
@@ -1,20 +1,77 @@
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "@/shared/ui/LinkUI";
 import { memo } from "react";
+import { useGetCurrentMenu } from "@/features/admin/sidebar/hook/useGetCurrentMenu";
+import { styled } from "@mui/material/styles";
+
+const StyledBreadcrumb = styled(Link, {
+  shouldForwardProp: (prop) => prop !== "isActive",
+})<{ isActive?: boolean }>(() => ({
+  display: "flex",
+  alignItems: "center",
+  maxWidth: "13rem",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  transition: "all 0.3s ease-in-out;",
+}));
 
 const BreadCrumbArea = memo(() => {
+  // const { pathname } = useLocation();
+
+  // const isErrorPage = pathname === "/error";
+
+  const { currentMenuTree } = useGetCurrentMenu();
+
   return (
-    <ol className="breadcrumb bg-transparent breadcrumb-sm pl-0 pr-0">
-      <li className="breadcrumb-item">
-        <a href="#">
-          <i className="fal fa-home mr-1"></i> Home
-        </a>
-      </li>
-      <li className="breadcrumb-item">
-        <a href="#">해피페이머니</a>
-      </li>
-      <li className="breadcrumb-item active">한도 관리</li>
-    </ol>
+    <Breadcrumbs
+      aria-label="breadcrumb"
+      sx={{
+        mb: "1.28rem",
+        fontSize: "1.1rem",
+        color: "grey.300",
+      }}
+    >
+      <StyledBreadcrumb underline="hover" to="/">
+        <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+        Home
+      </StyledBreadcrumb>
+
+      {currentMenuTree.map(({ label, menuUrl }, idx) => {
+        if (currentMenuTree.length - 1 === idx) {
+          return (
+            <Typography
+              key={`${label}-${idx}`}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                fontSize: "1.1rem",
+                maxWidth: "13rem",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                transition: "all 0.3s ease-in-out;",
+              }}
+            >
+              {label}
+            </Typography>
+          );
+        }
+        return (
+          <StyledBreadcrumb
+            key={`${label}-${idx}`}
+            underline="hover"
+            href={menuUrl ?? "#"}
+          >
+            {label}
+          </StyledBreadcrumb>
+        );
+      })}
+    </Breadcrumbs>
   );
 });
-
 BreadCrumbArea.displayName = "BreadCrumbArea";
+
 export default BreadCrumbArea;
