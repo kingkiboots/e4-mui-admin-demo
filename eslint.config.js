4f60324e26fdf7c57bb11834f2be1751db43f01a
kingkiboots <tuffhr4@gmail.com>
2025-10-10 16:19:51 +0900

    Fix: react-hooks/exhaustive-deps 규칙 off
    hook 에 빈 의존성 배열이 들어갈 때가 다수 있음

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default defineConfig([
     },
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
+      "react-hooks/exhaustive-deps": "off",
       "no-restricted-imports": [
         "error",
         {
