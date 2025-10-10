2d23994f73c9c0b50e8d468197b404bf0a877a51
kingkiboots <tuffhr4@gmail.com>
2025-10-10 16:36:00 +0900

    Feat: api mocker 기본 형태와 적용하는 부분 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,10 @@ import { appRouter } from "./appRouter";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import GlobalStyle from "./styles/GlobalStyle";
+import appMockWorker from "./appMockWorker";
+
+// api mock 데이터 등록
+appMockWorker();
 
 function App() {
   return (
