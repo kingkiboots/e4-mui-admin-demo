1abfc0f528c666cc8a1d3f91167a1aa119e8a543
kingkiboots <tuffhr4@gmail.com>
2025-10-10 16:20:43 +0900

    Feat: http 응답 상태코드 상수 추가

---
+++ src/shared/const/httpConst.ts
@@ -0,0 +1,7 @@
+/** HTTP 응답 상태 코드 */
+export const HTTP_RESPONSE_STATUS_SUCCESS = 200 as const;
+export const HTTP_RESPONSE_STATUS_BAD_REQUEST = 400 as const;
+export const HTTP_RESPONSE_STATUS_UNAUTHORIZED = 401 as const;
+export const HTTP_RESPONSE_STATUS_FORBIDDEN = 403 as const;
+export const HTTP_RESPONSE_STATUS_NOT_FOUND = 404 as const;
+export const HTTP_RESPONSE_STATUS_INTERNAL_SERVER_ERROR = 500 as const;
--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,1 +1,2 @@
 export * from "./uiConst";
+export * from "./httpConst";
