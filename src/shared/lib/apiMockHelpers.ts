cfc10e5e582b06bd08e4c353ccae44143407f97e
kingkiboots <tuffhr4@gmail.com>
2025-10-10 17:02:23 +0900

    Feat: api mocking 함수 작성 시의 헬퍼함수가 있는 apiMockHelpers 추가

---
+++ src/shared/lib/apiMockHelpers.ts
@@ -0,0 +1,49 @@
+import { AxiosError } from "axios";
+import { type AxiosRequestConfig } from "axios";
+
+type CallbackResponseSpecFunc = (
+  config: AxiosRequestConfig
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+) => any[] | Promise<any[]>;
+
+export const withDelay =
+  (
+    delay: number,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    response: { status: number; data: any }
+  ): CallbackResponseSpecFunc =>
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  (_config: AxiosRequestConfig) => {
+    //NOTE - reject는 deprecated 되었음. 에러를 응답하려면 4-500 응답코드로 응답할 것
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        resolve([response.status, response.data]);
+      }, delay);
+    });
+  };
+
+export class CustomAxiosError extends Error {
+  constructor(message: string) {
+    super(message); // (1)
+    this.name = "CustomAxiosError"; // (2)
+  }
+}
+
+export const createAxiosError = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  config: any,
+  status: number,
+  message: string
+): AxiosError => {
+  const error = new AxiosError(message);
+  error.config = config;
+  error.response = {
+    data: { message },
+    status,
+    statusText: message,
+    headers: {},
+    config,
+  };
+  // error.isAxiosError = true;
+  return error;
+};
