333e9352b689c5790f1b379cf1a2587ddfbd4fd0
kihyeon.kim <tuffhr4@gmail.com>
2025-10-13 18:02:05 +0900

    Feat: Searchbar 컴포넌트 추가
    기존 방식대로 obj[] 줘서 하는 방법 + ccp 패턴 둘다 가능하게끔 했음
    ccp 는 테스트중이며 obj는 추후 작업 예정

--- src/shared/ui/SearchbarUI.tsx
+++ src/shared/ui/SearchbarUI.tsx
@@ -1,10 +1,23 @@
 import MUIPaper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import { Row } from "./RowUI";
-import Grid, { type GridBaseProps } from "@mui/material/Grid";
-import type { ChangeEvent } from "react";
 
-const SearchContent = styled(MUIPaper)(({ theme }) => ({
+import {
+  createContext,
+  useContext,
+  type ChangeEvent,
+  type MouseEventHandler,
+  type PropsWithChildren,
+} from "react";
+import { type ColProps } from "./ColUI";
+import React from "react";
+import { isNull, isNullOrEmpty } from "../lib/commonHelpers";
+import { ButtonGroup, type ButtonGroupProps } from "./ButtonGroupUI";
+import Grid from "@mui/material/Grid";
+
+const SearchContent = styled(MUIPaper, {
+  label: "search-content",
+})(({ theme }) => ({
   ...theme.typography.body2,
   textAlign: "center",
   color: theme.palette.text.secondary,
@@ -13,27 +26,98 @@ const SearchContent = styled(MUIPaper)(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   position: "relative",
   borderRadius: "4px",
-  border: `2px solid ${theme.palette.grey[50]}`,
+  border: `2px solid ${theme.palette.grey[100]}`,
 }));
 
-interface SearchbarProps {
-  //   inputsDef: InputsDefProps[];
-  searchBarSize?: GridBaseProps["size"];
-  onChange: (e: ChangeEvent) => void;
-  //   buttonsDef?: SearchButtonsDefProps;
+interface SearchbarContextValue {
+  searchBarSize: ColProps["size"];
 }
-export const Searchbar = ({ searchBarSize = 10 }: SearchbarProps) => {
+
+const SearchbarContext = createContext<SearchbarContextValue | null>(null);
+
+interface SearchbarProps {
+  // inputsDef: InputsDefProps[];
+  inputsDef?: { name: string; value: string; type: string }[];
+  searchBarSize?: ColProps["size"];
+  onChange: (e: ChangeEvent) => void;
+  // buttonsDef?: SearchButtonsDefProps;
+  buttonsDef?: { search: MouseEventHandler<HTMLButtonElement> };
+  children?: React.ReactNode;
+}
+type SearchbarSubComponents = {
+  InputsArea: typeof InputsArea;
+  ButtonsArea: typeof ButtonsArea;
+};
+export const Searchbar: React.FC<SearchbarProps> & SearchbarSubComponents = ({
+  searchBarSize = 10,
+  inputsDef,
+  buttonsDef,
+  children,
+}) => {
+  const { inputsArea, buttonsArea } = (() => {
+    const childrenArray = React.Children.toArray(children);
+
+    return {
+      inputsArea: isNullOrEmpty(inputsDef)
+        ? (childrenArray.find(
+            (child) =>
+              React.isValidElement(child) &&
+              child.type.toString() === Searchbar.InputsArea.toString()
+          ) as React.ReactElement<InputAreaProps> | undefined)
+        : undefined,
+      buttonsArea: isNullOrEmpty(buttonsDef)
+        ? (childrenArray.find(
+            (child) =>
+              React.isValidElement(child) &&
+              child.type.toString() === Searchbar.ButtonsArea.toString()
+          ) as React.ReactElement<ButtonsAreaProps> | undefined)
+        : undefined,
+    };
+  })();
+
   return (
-    <SearchContent elevation={0}>
-      <Row>
+    <SearchbarContext.Provider value={{ searchBarSize }}>
+      <SearchContent elevation={0}>
         <Grid container>
-          <Grid size={searchBarSize}></Grid>
-          <Grid
-            size={12 - Number(searchBarSize)}
-            sx={{ textAlign: "right" }}
-          ></Grid>
+          {/* input */}
+          {isNull(inputsArea)
+            ? inputsDef?.map(() => <InputsArea></InputsArea>)
+            : inputsArea}
+          {/* button */}
+          {/* {isNull(buttonsArea) ? <ButtonsArea></ButtonsArea> : buttonsArea} */}
+          {buttonsArea}
         </Grid>
-      </Row>
-    </SearchContent>
+      </SearchContent>
+    </SearchbarContext.Provider>
   );
 };
+
+interface InputAreaProps extends PropsWithChildren {}
+const InputsArea = ({ children }: InputAreaProps) => {
+  const context = useContext(SearchbarContext);
+  if (!context)
+    throw new Error("SearchbarInputs must be used within Searchbar");
+
+  return (
+    <Grid size={context.searchBarSize}>
+      <Row>{children}</Row>
+    </Grid>
+  );
+};
+
+interface ButtonsAreaProps {
+  children: ButtonGroupProps["children"];
+}
+const ButtonsArea = ({ children }: ButtonsAreaProps) => {
+  const context = useContext(SearchbarContext);
+  if (!context)
+    throw new Error("SearchbarButtons must be used within Searchbar");
+  return (
+    <Grid size={12 - Number(context.searchBarSize)} textAlign="right">
+      <ButtonGroup>{children}</ButtonGroup>
+    </Grid>
+  );
+};
+
+Searchbar.InputsArea = InputsArea;
+Searchbar.ButtonsArea = ButtonsArea;
