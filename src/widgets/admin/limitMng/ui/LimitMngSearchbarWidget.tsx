bba2d2d940876918f13e1371ebfeefaa73b1747c
kihyeon.kim <tuffhr4@gmail.com>
2025-10-13 18:02:34 +0900

    Feat: 한도 관리 SearchbarWidget ui만 그려 놓음

--- src/widgets/admin/limitMng/ui/LimitMngSearchbarWidget.tsx
+++ src/widgets/admin/limitMng/ui/LimitMngSearchbarWidget.tsx
@@ -1,7 +1,37 @@
+import { Button } from "@/shared/ui/ButtonUI";
 import { Searchbar } from "@/shared/ui/SearchbarUI";
+import { Select, type SelectOption } from "@/shared/ui/SelectUI";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const LimitMngSearchbarWidget = () => {
-  return <Searchbar onChange={() => {}} />;
+  const options: SelectOption[] = [
+    { value: "1", label: "하나" },
+    { value: "2", label: "둘" },
+    { value: "3", label: "셋" },
+  ];
+  return (
+    <Searchbar onChange={() => {}}>
+      <Searchbar.InputsArea>
+        <Select
+          label="한도구분"
+          options={options}
+          defaultValue={"1"}
+          labelColSpan={{ xs: 12, sm: 4 }}
+          inputColSpan={{ xs: 12, sm: 8 }}
+        />
+      </Searchbar.InputsArea>
+      <Searchbar.ButtonsArea>
+        <Button size="small" color="info" variant="contained">
+          <SearchIcon />
+          &nbsp; 조회
+        </Button>
+        <Button size="small" color="dark" variant="contained">
+          <ClearIcon fontSize="small" />
+        </Button>
+      </Searchbar.ButtonsArea>
+    </Searchbar>
+  );
 };
 
 export default LimitMngSearchbarWidget;
