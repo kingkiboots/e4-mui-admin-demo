472290bb53a87806f167e2f17bc86a224e566395
kihyeon.kim <tuffhr4@gmail.com>
2025-10-13 17:57:27 +0900

    Feat: 공통 타입 추가한 것 public api 에 추가

--- src/shared/type/index.ts
+++ src/shared/type/index.ts
@@ -3,3 +3,4 @@ export * from "./mui-types";
 export * from "./react-router-dom-types";
 export * from "./axios-types";
 export * from "./session-storage-share-types";
+export * from "./common-types";
