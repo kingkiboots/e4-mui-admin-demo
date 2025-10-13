c202b3f3492510db04d88181539c5bada487e616
kihyeon.kim <tuffhr4@gmail.com>
2025-10-13 17:59:29 +0900

    Feat: Col 컴포넌트 추가

---
+++ src/shared/ui/ColUI.tsx
@@ -0,0 +1,19 @@
+import Grid, { type GridProps } from "@mui/material/Grid";
+
+import { styled } from "@mui/material/styles";
+
+const Cell = styled(Grid, {
+  label: "col",
+})({
+  position: "relative",
+  width: "100%",
+  paddingRight: "1.2rem",
+  paddingLeft: "1.2rem",
+});
+
+export interface ColProps extends Omit<GridProps, "container" | "size"> {
+  size: GridProps["size"];
+}
+export const Col: React.FC<ColProps> = (props) => {
+  return <Cell {...props} />;
+};
