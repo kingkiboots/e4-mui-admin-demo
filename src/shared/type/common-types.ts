54353ec69e1fd87a1d9bc464bf0a1a82110ef9d6
kihyeon.kim <tuffhr4@gmail.com>
2025-10-13 10:39:35 +0900

    Feat: 편의상 추가로 정의한 타입들인 common-types 추가

---
+++ src/shared/type/common-types.ts
@@ -0,0 +1,11 @@
+export type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends (infer ElementType)[] ? ElementType : never;
+export type YNType = "Y" | "N";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ArgumentTypes<F extends (...args: any) => unknown> = F extends (
+  ...args: infer A
+) => // eslint-disable-next-line @typescript-eslint/no-explicit-any
+any
+  ? A
+  : never;
