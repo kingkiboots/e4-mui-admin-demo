22d235fc7d47c6b8cc0ba8de6885682d2ae2268f
kihyeon.kim <tuffhr4@gmail.com>
2025-10-14 17:02:02 +0900

    Feat: VALIDATION_MSG 상수 추가

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -3,3 +3,4 @@ export * from "./httpConst";
 export * from "./sessionStorageShareConst";
 export * from "./browserStorageConst";
 export * from "./apiConst";
+export * from "./validationMsgConst";
---
+++ src/shared/const/validationMsgConst.ts
@@ -0,0 +1,1 @@
+export const VALIDATION_MSG_UNVALID_REQUIRED = "필수 입력항목입니다." as const;
