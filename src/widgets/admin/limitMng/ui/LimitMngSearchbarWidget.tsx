3e200c3010f13dcf00268e1c969a9b2c197a2a8a
kihyeon.kim <tuffhr4@gmail.com>
2025-10-14 17:03:40 +0900

    Feat: 한도관리 페이지에 추가 데이트피커 예시 추가

--- src/widgets/admin/limitMng/ui/LimitMngSearchbarWidget.tsx
+++ src/widgets/admin/limitMng/ui/LimitMngSearchbarWidget.tsx
@@ -5,6 +5,13 @@ import SearchIcon from "@mui/icons-material/Search";
 import ClearIcon from "@mui/icons-material/Clear";
 import { TextField } from "@/shared/ui/TextFieldUI";
 import { useForm } from "react-hook-form";
+import { DatePickerField } from "@/shared/ui/DatePickerFieldUI";
+import type { Dayjs } from "dayjs";
+
+interface SearchFormData {
+  account: string;
+  startDate: Dayjs | null;
+}
 
 const LimitMngSearchbarWidget = () => {
   const options: SelectOption[] = [
@@ -13,7 +20,7 @@ const LimitMngSearchbarWidget = () => {
     { value: "3", label: "셋" },
   ];
 
-  const { register } = useForm<{ account: string }>();
+  const { register, control } = useForm<SearchFormData>();
 
   return (
     <Searchbar onChange={() => {}}>
@@ -25,7 +32,23 @@ const LimitMngSearchbarWidget = () => {
           labelColSpan={{ xs: 12, sm: 4 }}
           inputColSpan={{ xs: 12, sm: 8 }}
         />
-        <TextField label="계좌번호" register={register("account")} />
+        <TextField
+          label="계좌번호"
+          register={register("account", {
+            required: true,
+          })}
+          required
+        />
+        <DatePickerField
+          name="startDate"
+          control={control}
+          dateTimeType="start"
+          label="시작일"
+          placeholder="날짜 선택"
+          rules={{
+            required: true,
+          }}
+        />
       </Searchbar.InputsArea>
       <Searchbar.ButtonsArea>
         <Button size="small" color="info" variant="contained">
