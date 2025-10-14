47aa45c4a8889c1e21d81e17b63c857abac2041f
kihyeon.kim <tuffhr4@gmail.com>
2025-10-14 18:23:47 +0900

    Feat: MSA 방송푸시 메시지 관리 화면 추가

--- src/pages/admin/pushMsgMng/ui/PushMsgMngPage.tsx
+++ src/pages/admin/pushMsgMng/ui/PushMsgMngPage.tsx
@@ -1,10 +1,12 @@
 import PageContentLayout from "@/widgets/admin/common/ui/PageContentLayout";
 import Meta from "./Meta";
+import PushMsgMngSearchbarWidget from "@/widgets/admin/pushMsgMng/ui/PushMsgMngSearchbarWidget";
 
 const PushMsgMngPage = () => {
   return (
     <Meta>
       <PageContentLayout>
+        <PushMsgMngSearchbarWidget />
       </PageContentLayout>
     </Meta>
   );
---
+++ src/widgets/admin/pushMsgMng/ui/PushMsgMngSearchbarWidget.tsx
@@ -0,0 +1,78 @@
+import { DatePickerField } from "@/shared/ui/DatePickerFieldUI";
+import type { SearchbarButtonGroupProps } from "@/shared/ui/SearchbarButtonGroupUI";
+import { Searchbar } from "@/shared/ui/SearchbarUI";
+import { Select, type SelectOption } from "@/shared/ui/SelectUI";
+import { TextField } from "@/shared/ui/TextFieldUI";
+import type { Dayjs } from "dayjs";
+import { memo } from "react";
+import { useForm } from "react-hook-form";
+
+interface SearchFormData {
+  account: string;
+  startDate: Dayjs | null;
+}
+
+const PushMsgMngSearchbarWidget = memo(() => {
+  const options: SelectOption[] = [
+    { value: "1", label: "하나" },
+    { value: "2", label: "둘" },
+    { value: "3", label: "셋" },
+  ];
+
+  const { register, control } = useForm<SearchFormData>();
+
+  const buttonsDef: SearchbarButtonGroupProps = {
+    onClickSearch: () => {
+      console.log("부다");
+    },
+    onClickClearSearchbar: () => {
+      console.log("페슽");
+    },
+  };
+
+  return (
+    <Searchbar buttonsDef={buttonsDef}>
+      <Searchbar.InputsArea>
+        <Select
+          label="한도구분"
+          options={options}
+          defaultValue={"1"}
+          labelColSpan={{ xs: 12, sm: 4 }}
+          inputColSpan={{ xs: 12, sm: 8 }}
+        />
+        <TextField
+          label="계좌번호"
+          labelColSpan={{ xs: 12, sm: 4 }}
+          inputColSpan={{ xs: 12, sm: 8 }}
+          register={register("account", {
+            required: true,
+          })}
+          required
+        />
+        <DatePickerField
+          labelColSpan={{ xs: 12, sm: 4 }}
+          inputColSpan={{ xs: 12, sm: 8 }}
+          name="startDate"
+          control={control}
+          dateTimeType="start"
+          label="시작일"
+          placeholder="날짜 선택"
+          rules={{
+            required: true,
+          }}
+        />
+      </Searchbar.InputsArea>
+      {/* <Searchbar.ButtonsArea>
+        <Button size="small" color="info" variant="contained">
+          <SearchIcon />
+          &nbsp; 조회
+        </Button>
+        <Button size="small" color="dark" variant="contained">
+          <ClearIcon fontSize="small" />
+        </Button>
+      </Searchbar.ButtonsArea> */}
+    </Searchbar>
+  );
+});
+
+export default PushMsgMngSearchbarWidget;
