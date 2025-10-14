2fe5bed371bfff20e488829f69c6e6e35a6f6d35
SIULEE <e4.siu.lee@gmail.com>
2025-10-14 11:55:17 +0900

    pages 기본 폴더 수조 생성

---
+++ src/pages/admin/eventMngPage/ui/EventMngPage.tsx
@@ -0,0 +1,14 @@
+import PageContentLayout from "@/widgets/admin/common/ui/PageContentLayout";
+import Meta from "./Meta";
+
+const EventMngPage = () => {
+  return (
+    <Meta>
+      <PageContentLayout>
+
+      </PageContentLayout>
+    </Meta>
+  );
+};
+
+export default EventMngPage;
