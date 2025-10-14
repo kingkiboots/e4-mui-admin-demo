ca7211227d417057deea735e2d1a712ce8e9aa34
kihyeon.kim <tuffhr4@gmail.com>
2025-10-14 15:59:51 +0900

    Refactor: date picker 개발을 위해 ThemeProvider에 Localization Provider 추가 그리고 GlobalStyle도 관심사분리르르 위해 이곳으로 이동

--- src/app/provider/ThemeProvider.tsx
+++ src/app/provider/ThemeProvider.tsx
@@ -1,10 +1,20 @@
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import type { PropsWithChildren } from "react";
 import type React from "react";
+import GlobalStyle from "../styles/GlobalStyle";
 import themeConfig from "../theme/themeConfig";
 
 const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  return <MuiThemeProvider theme={themeConfig}>{children}</MuiThemeProvider>;
+  return (
+    <MuiThemeProvider theme={themeConfig}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
+        <GlobalStyle />
+        {children}
+      </LocalizationProvider>
+    </MuiThemeProvider>
+  );
 };
 
 export default ThemeProvider;
