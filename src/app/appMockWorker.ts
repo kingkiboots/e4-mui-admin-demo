9f102ca097ca140e35c8b827cea250904f386de6
kingkiboots <tuffhr4@gmail.com>
2025-10-14 21:59:29 +0900

    Feat: menuList api mock 등록

--- src/app/appMockWorker.ts
+++ src/app/appMockWorker.ts
@@ -1,1 +1,6 @@
-export default () => {};
+import { menuApiMockHandler } from "@/entities/admin/menu/api/__mocks__/menuApiMockHandler";
+import { apiClientMocker } from "@/shared/api/netInstances";
+
+export default () => {
+  menuApiMockHandler(apiClientMocker);
+};
