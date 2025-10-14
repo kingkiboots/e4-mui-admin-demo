ad04e865eb10cbd9c17f641b70614aa16c1bf84b
kihyeon.kim <tuffhr4@gmail.com>
2025-10-14 13:14:52 +0900

    Refactor: GlobalStyle의 css override는 themeConfig.components.ts로 이관

--- src/app/styles/GlobalStyle.tsx
+++ src/app/styles/GlobalStyle.tsx
@@ -1,41 +1,10 @@
 import CssBaseline from "@mui/material/CssBaseline";
-import GlobalStyles from "@mui/material/GlobalStyles";
 import { memo } from "react";
 
 const GlobalStyle = memo(() => {
   return (
     <>
       <CssBaseline />
-      <GlobalStyles
-        styles={(theme) => ({
-          "*": {
-            boxSizing: "border-box",
-            margin: 0,
-            padding: 0,
-          },
-          html: {
-            fontSize: "62.5%",
-            direction: "ltr",
-            textRendering: "optimizeLegibility",
-          },
-          body: {
-            letterSpacing: "0.1px",
-            backgroundColor: "#fff",
-            fontSize: "1.3rem",
-          },
-          "a:link,a:visited": {
-            textDecoration: "none",
-            color: theme.palette.text.primary,
-            cursor: "pointer",
-            backgroundColor: "transparent",
-            touchAction: "manipulation",
-          },
-          "a:hover,a:active": {},
-          "ul, ol": {
-            listStyle: "none",
-          },
-        })}
-      />
     </>
   );
 });
