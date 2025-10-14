fe0a0f9fb7c786b2239bc44b08c0a50af28d4ee8
kihyeon.kim <tuffhr4@gmail.com>
2025-10-14 13:18:21 +0900

    Fix: app 레이어는 다 export default 라서 themeConfig export 방식도 변경된 것 적용

--- src/app/provider/ThemeProvider.tsx
+++ src/app/provider/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import type { PropsWithChildren } from "react";
 import type React from "react";
-import { themeConfig } from "../theme/themeConfig";
+import themeConfig from "../theme/themeConfig";
 
 const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return <MuiThemeProvider theme={themeConfig}>{children}</MuiThemeProvider>;
