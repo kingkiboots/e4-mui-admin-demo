8d5db260970811fba5885efda60571d0c83b56c4
kihyeon.kim <tuffhr4@gmail.com>
2025-10-15 09:34:22 +0900

    Feat: 브레드 크럼브도 useQuery 적용 및 스켈레톤 적용

--- src/features/admin/sidebar/hook/useGetCurrentMenu.ts
+++ src/features/admin/sidebar/hook/useGetCurrentMenu.ts
@@ -1,16 +1,31 @@
+import { useGetMenuListQuery } from "@/entities/admin/menu/lib/useMenuApiQueries";
+import type { MenuList } from "@/entities/admin/menu/types";
+import type { ApiAxiosResponse } from "@/shared/type";
+import { useCallback } from "react";
 import { useLocation } from "react-router-dom";
-import navigationConfig from "@/entities/admin/menu/config/menus.json";
-import { useMemo } from "react";
 import { getCurrentMenuTree } from "../model/handleFindActiveMenuId";
 
 export const useGetCurrentMenu = () => {
   const { pathname } = useLocation();
 
-  const currentMenuTree = useMemo(() => {
-    return getCurrentMenuTree(pathname, navigationConfig) ?? [];
-  }, [pathname]);
+  const select = useCallback(
+    (res: ApiAxiosResponse<MenuList>) => {
+      const data = res.data.data;
+      if (!data) {
+        return [];
+      }
+
+      return getCurrentMenuTree(pathname, data) ?? [];
+    },
+    [pathname]
+  );
+
+  const { data: currentMenuTree, ...query } = useGetMenuListQuery({
+    select,
+  });
 
   return {
     currentMenuTree,
+    ...query,
   };
 };
--- src/widgets/admin/breadCrumb/ui/BreadCrumbArea.tsx
+++ src/widgets/admin/breadCrumb/ui/BreadCrumbArea.tsx
@@ -6,6 +6,7 @@ import { memo } from "react";
 import { useGetCurrentMenu } from "@/features/admin/sidebar/hook/useGetCurrentMenu";
 import { styled } from "@mui/material/styles";
 import { isNullOrEmpty } from "@/shared/lib/commonHelpers";
+import Skeleton from "@mui/material/Skeleton";
 
 const StyledBreadcrumb = styled(Link, {
   shouldForwardProp: (prop) => prop !== "isActive",
@@ -24,7 +25,7 @@ const BreadCrumbArea = memo(() => {
 
   // const isErrorPage = pathname === "/error";
 
-  const { currentMenuTree } = useGetCurrentMenu();
+  const { currentMenuTree, isPending } = useGetCurrentMenu();
 
   return (
     <Breadcrumbs
@@ -40,41 +41,55 @@ const BreadCrumbArea = memo(() => {
         Home
       </StyledBreadcrumb>
 
-      {currentMenuTree.map(({ label, menuUrl, id }, idx) => {
-        if (currentMenuTree.length - 1 === idx) {
-          return (
-            <Typography
-              key={`${label}-${idx}`}
+      {isPending
+        ? new Array(2).fill(null).map((_, idx) => (
+            <Skeleton
+              key={idx}
+              variant="text"
+              animation="pulse"
               sx={{
                 display: "flex",
                 alignItems: "center",
-                fontSize: "1.1rem",
+                width: "13rem",
                 maxWidth: "13rem",
-                textOverflow: "ellipsis",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                transition: "all 0.3s ease-in-out;",
-
-                "&:hover": {
-                  maxWidth: "25rem !important",
-                },
               }}
-            >
-              {label}
-              {isNullOrEmpty(id) ? "" : `[${id}]`}
-            </Typography>
-          );
-        }
-        return (
-          <StyledBreadcrumb
-            key={`${label}-${idx}`}
-            underline="hover"
-            href={menuUrl ?? "#"}
-          >
-            {label}
-          </StyledBreadcrumb>
-        );
-      })}
+            />
+          ))
+        : currentMenuTree?.map(({ label, menuUrl, id }, idx) => {
+            if (currentMenuTree.length - 1 === idx) {
+              return (
+                <Typography
+                  key={`${label}-${idx}`}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    fontSize: "1.1rem",
+                    maxWidth: "13rem",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    transition: "all 0.3s ease-in-out;",
+
+                    "&:hover": {
+                      maxWidth: "25rem !important",
+                    },
+                  }}
+                >
+                  {label}
+                  {isNullOrEmpty(id) ? "" : `[${id}]`}
+                </Typography>
+              );
+            }
+            return (
+              <StyledBreadcrumb
+                key={`${label}-${idx}`}
+                underline="hover"
+                href={menuUrl ?? "#"}
+              >
+                {label}
+              </StyledBreadcrumb>
+            );
+          })}
     </Breadcrumbs>
   );
 });
