16fc5fd6f0e8b6708e002beec326859ffad54db2
kihyeon.kim <tuffhr4@gmail.com>
2025-10-15 16:51:46 +0900

    Comment: appMockWorker에 주석 작성

--- src/app/appMockWorker.ts
+++ src/app/appMockWorker.ts
@@ -4,6 +4,7 @@ import { prodcutMngApiMockHandler } from "@/entities/admin/productMng/api/__mock
 import { pushMsgMngApiMockHandler } from "@/entities/admin/pushMsgMng/api/__mocks__/pushMsgMngApiMockHandler";
 import { apiClientMocker } from "@/shared/api/netInstances";
 
+//NOTE - 여기에 등록하면 api 보다 우선시 되니 api 만들어지면 해당 mock handler 지울것!
 export default () => {
   menuApiMockHandler(apiClientMocker);
   pushMsgMngApiMockHandler(apiClientMocker);
