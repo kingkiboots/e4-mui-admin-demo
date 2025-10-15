f1fa8eb5d2fe9623dcec5bdaeb67e0c630b16872
kihyeon.kim <tuffhr4@gmail.com>
2025-10-15 11:49:25 +0900

    Feat: 패널 하단에 정보 알려주는 Information 컴포넌트 추가

---
+++ src/shared/ui/InformationUI.tsx
@@ -0,0 +1,29 @@
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import React, { memo, type ComponentProps } from "react";
+
+const InformationTypography = styled(Typography, {
+  label: "msg",
+  name: "InformationTypography",
+})({
+  fontSize: "1.2rme",
+  margin: "0 0 2rem 0",
+  marginTop: "-1rem",
+  padding: 0,
+  color: "grey.500",
+  lineHeight: "100%",
+
+  "&::before": {
+    content: '"\\002A "',
+  },
+});
+
+export interface PropsWithInformation {
+  information?: React.ReactNode;
+}
+
+export const Information = memo<ComponentProps<typeof InformationTypography>>(
+  (props) => {
+    return <InformationTypography {...props} />;
+  }
+);
