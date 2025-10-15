a74c5aed2bcc0681daf472b320d131d54d34da93
kihyeon.kim <tuffhr4@gmail.com>
2025-10-15 18:00:21 +0900

    Feat: ButtonGroupRow 컴포넌트 추가

---
+++ src/shared/ui/ButtonGroupRowUI.tsx
@@ -0,0 +1,15 @@
+import { type PropsWithChildren } from "react";
+import { ButtonGroup } from "./ButtonGroupUI";
+import { Col } from "./ColUI";
+import { Row } from "./RowUI";
+
+interface ButtonGroupRowProps extends PropsWithChildren {}
+export const ButtonGroupRow = ({ children }: ButtonGroupRowProps) => {
+  return (
+    <Row marginBottom="0.5rem">
+      <Col size={12}>
+        <ButtonGroup>{children}</ButtonGroup>
+      </Col>
+    </Row>
+  );
+};
