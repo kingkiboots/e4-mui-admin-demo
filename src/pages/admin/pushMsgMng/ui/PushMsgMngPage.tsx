6d88d8795b0cb062f817c709a7d753121c8e60a4
kihyeon.kim <tuffhr4@gmail.com>
2025-10-15 11:51:39 +0900

    Feat: datagrid ui 예시 일단 구현

--- src/pages/admin/pushMsgMng/ui/PushMsgMngPage.tsx
+++ src/pages/admin/pushMsgMng/ui/PushMsgMngPage.tsx
@@ -1,12 +1,14 @@
 import PageContentLayout from "@/widgets/admin/common/ui/PageContentLayout";
 import Meta from "./Meta";
 import PushMsgMngSearchbarWidget from "@/widgets/admin/pushMsgMng/ui/PushMsgMngSearchbarWidget";
+import { PushMsgMngMsgListWidget } from "@/widgets/admin/pushMsgMng/ui/PushMsgMngMsgListWidget";
 
 const PushMsgMngPage = () => {
   return (
     <Meta>
       <PageContentLayout>
         <PushMsgMngSearchbarWidget />
+        <PushMsgMngMsgListWidget />
       </PageContentLayout>
     </Meta>
   );
