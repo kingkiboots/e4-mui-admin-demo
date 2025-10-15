9f024f7b62a48c2d3c73bbc96f8dac58066e5fd4
SIULEE <e4.siu.lee@gmail.com>
2025-10-15 16:31:32 +0900

    fixed : DetailUi Props 변경 적용

--- src/shared/ui/DetailUI.tsx
+++ src/shared/ui/DetailUI.tsx
@@ -1,15 +1,9 @@
-import { Panel } from "./PanelUI";
+import type { PropsWithChildren } from "react";
+import { Panel, type PropsWithPanelProps } from "./PanelUI";
 import { Row } from "./RowUI";
 
-export const Detail = ({
-  title,
-  information,
-  children,
-}: {
-  title: string;
-  information: string;
-  children: any;
-}) => {
+interface DetailProps extends PropsWithPanelProps, PropsWithChildren {}
+export const Detail = ({ title, information, children }: DetailProps) => {
   return (
     <Panel title={title} information={information}>
       <Row>{children}</Row>
