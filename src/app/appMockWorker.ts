e674399b5d08bfd718ce141c5bd0fbdd3ba9c5c0
SIULEE <e4.siu.lee@gmail.com>
2025-10-16 09:46:33 +0900

    목업 복구

--- src/app/appMockWorker.ts
+++ src/app/appMockWorker.ts
@@ -1,7 +1,9 @@
 import { menuApiMockHandler } from "@/entities/admin/menu/api/__mocks__/menuApiMockHandler";
+import { pushMsgMngApiMockHandler } from "@/entities/admin/pushMsgMng/api/__mocks__/pushMsgMngApiMockHandler";
 import { apiClientMocker } from "@/shared/api/netInstances";
 
 //NOTE - 여기에 등록하면 api 보다 우선시 되니 api 만들어지면 해당 mock handler 지울것!
 export default () => {
   menuApiMockHandler(apiClientMocker);
+  pushMsgMngApiMockHandler(apiClientMocker);
 };
