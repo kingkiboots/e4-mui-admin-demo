9c71a552203eaae8070f833ca890038bc9e64e86
kingkiboots <tuffhr4@gmail.com>
2025-10-18 23:28:17 +0900

    Comment: 푸쉬 목록 관리 타입에 각 항목 주석 작성

--- src/entities/admin/pushMsgMng/types/push-msg-mng-api-types.ts
+++ src/entities/admin/pushMsgMng/types/push-msg-mng-api-types.ts
@@ -1,28 +1,43 @@
+/**
+ * 푸쉬 목록 조회 파라미터
+ */
 export type PushMsgListSearchData = {
-  serviceCd: string;
-  seq: string;
+  serviceCd: string /* UMS 서비스코드 */;
+  seq: string /* 푸시 일련번호 */;
 };
 
+/**
+ * 푸쉬 데이터
+ */
 export type PushMsgData = {
-  serviceCd: string;
-  seq: string;
-  name: string;
-  content: string;
-  url: string;
-  useYn: string;
+  serviceCd: string /* UMS 서비스코드 */;
+  seq: string /* 푸시 일련번호 */;
+  name: string /* 푸쉬 제목 */;
+  content: string /* 푸쉬 내용 */;
+  url: string /* 푸쉬 URL */;
+  useYn: string /* 사용 여부 */;
 };
 
+/**
+ * 푸쉬 목록 데이터
+ */
 export type PushMsgList = PushMsgData[];
 
+/**
+ * 푸쉬 상세 데이터
+ */
 export type PushMsgDetailData = {
-  serviceCd?: string;
-  seq?: string;
-  name?: string;
-  content?: string;
-  url?: string;
-  useYn?: string;
+  serviceCd?: string /* UMS 서비스코드 */;
+  seq?: string /* 푸시 일련번호 */;
+  name?: string /* 푸쉬 제목 */;
+  content?: string /* 푸쉬 내용 */;
+  url?: string /* 푸쉬 URL */;
+  useYn?: string /* 사용 여부 */;
 };
 
+/**
+ * 푸쉬 데이터 삭제 파라미터
+ */
 export type PushMsgDeleteParam = {
-  serviceCd: string;
+  serviceCd: string /* UMS 서비스코드 */;
 };
