7e54c6345f18086bced07058306944e3185f1b05
kingkiboots <tuffhr4@gmail.com>
2025-10-18 23:44:28 +0900

    Feat: Mock Api 파라미터도 확인할 수 있는 방식으로 변경
    조회해서 필터링도 해야하므로

--- src/entities/admin/pushMsgMng/api/__mocks__/pushMsgMngApiMockHandler.ts
+++ src/entities/admin/pushMsgMng/api/__mocks__/pushMsgMngApiMockHandler.ts
@@ -1,42 +1,37 @@
 import { restApiConfig } from "@/shared/config";
 import { withDelay } from "@/shared/lib/apiMockHelpers";
-import MockAdapter from "axios-mock-adapter";
-
-export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
-  mockInstance.onGet(restApiConfig.api.pushMsgMng.list).reply(
-    withDelay(0, {
-      status: 200,
-      data: {
-        code: "0000",
-        message: "정상 처리 되었습니다.",
-        timestamp: "20240726092207",
-        data: Array(10)
-          .fill(null)
-          .map((_, idx) => ({
-            serviceCd: String(115515451 + idx),
-            seq: String(5665456 + idx),
-            name: "	푸시제목 테스트" + idx,
-            content: "푸시내용 테스트" + idx,
-            url: "https://www.test.com/event/push?q=" + idx,
-            useYn: idx % 2 === 0 ? "Y" : "N",
-          })),
-      },
-    })
-  );
-};
-
-/*
-
-import { restApiConfig } from "@/shared/config";
-import { withDelay } from "@/shared/lib/apiMockHelpers";
 import type { AxiosRequestConfig } from "axios";
 import MockAdapter from "axios-mock-adapter";
 
+// export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
+//   mockInstance.onGet(restApiConfig.api.pushMsgMng.list).reply(
+//     withDelay(0, {
+//       status: 200,
+//       data: {
+//         code: "0000",
+//         message: "정상 처리 되었습니다.",
+//         timestamp: "20240726092207",
+//         data: Array(10)
+//           .fill(null)
+//           .map((_, idx) => ({
+//             serviceCd: String(115515451 + idx),
+//             seq: String(5665456 + idx),
+//             name: "	푸시제목 테스트" + idx,
+//             content: "푸시내용 테스트" + idx,
+//             url: "https://www.test.com/event/push?q=" + idx,
+//             useYn: idx % 2 === 0 ? "Y" : "N",
+//           })),
+//       },
+//     })
+//   );
+// };
+
+//NOTE - 받는 파라미터 확인할 수 있는 방식
 export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
   mockInstance
     .onGet(restApiConfig.api.pushMsgMng.list)
     .reply((config: AxiosRequestConfig) => {
-    console.log('config.params', config.params)
+      console.log("config.params", config.params);
       return withDelay(500, {
         status: 200,
         data: {
@@ -57,5 +52,3 @@ export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
       })(config);
     });
 };
-
-*/
