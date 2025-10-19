259f653cbce11ae74bdd2881b27ce8d641f1e209
kingkiboots <tuffhr4@gmail.com>
2025-10-19 16:00:11 +0900

    Feat: restApiConfig.api.pushMsgMng.list mock 함수에 검색기능 추가

--- src/entities/admin/pushMsgMng/api/__mocks__/pushMsgMngApiMockHandler.ts
+++ src/entities/admin/pushMsgMng/api/__mocks__/pushMsgMngApiMockHandler.ts
@@ -2,6 +2,8 @@ import { restApiConfig } from "@/shared/config";
 import { withDelay } from "@/shared/lib/apiMockHelpers";
 import type { AxiosRequestConfig } from "axios";
 import MockAdapter from "axios-mock-adapter";
+import type { PushMsgListSearchParams } from "../../types";
+import { isEmpty, isNullOrEmpty } from "@/shared/lib/commonHelpers";
 
 // export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
 //   mockInstance.onGet(restApiConfig.api.pushMsgMng.list).reply(
@@ -32,6 +34,10 @@ export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
     .onGet(restApiConfig.api.pushMsgMng.list)
     .reply((config: AxiosRequestConfig) => {
       console.log("config.params", config.params);
+      const { serviceCd = "", seq = "" } = (
+        isNullOrEmpty(config.params) ? {} : config.params
+      ) as PushMsgListSearchParams;
+
       return withDelay(500, {
         status: 200,
         data: {
@@ -47,7 +53,17 @@ export const pushMsgMngApiMockHandler = (mockInstance: MockAdapter) => {
               content: "푸시내용 테스트" + idx,
               url: "https://www.test.com/event/push?q=" + idx,
               useYn: idx % 2 === 0 ? "Y" : "N",
-            })),
+            }))
+            .filter((item) => {
+              return (
+                (isEmpty(serviceCd)
+                  ? item.serviceCd === item.serviceCd
+                  : item.serviceCd.startsWith(serviceCd)) &&
+                (isEmpty(seq)
+                  ? item.seq === item.seq
+                  : item.seq.startsWith(seq))
+              );
+            }),
         },
       })(config);
     });
