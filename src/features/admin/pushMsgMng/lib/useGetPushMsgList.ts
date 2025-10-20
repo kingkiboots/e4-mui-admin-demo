59236617e896c8d43a675eba32bc869e4b88a344
SIULEE <e4.siu.lee@gmail.com>
2025-10-20 11:52:23 +0900

    방송푸시 메시지 관리 데이터 변경 적용

--- src/features/admin/pushMsgMng/lib/useGetPushMsgList.ts
+++ src/features/admin/pushMsgMng/lib/useGetPushMsgList.ts
@@ -7,14 +7,36 @@ import type { ApiAxiosResponse } from "@/shared/type";
 import { useCallback } from "react";
 
 export const useGetPushMsgList = (params: PushMsgListSearchParams) => {
-  const select = useCallback((res: ApiAxiosResponse<PushMsgList>) => {
-    const data = res.data.data;
-    if (!data) {
-      return undefined;
-    }
+  const select = useCallback(
+    (res: ApiAxiosResponse<PushMsgList>) => {
+      const data = res.data.data;
+      if (!data) {
+        return undefined;
+      }
 
-    return data;
-  }, []);
+      if (params.serviceCd && params.seq) {
+        const filtered = data.filter(
+          (item) => item.serviceCd == params.serviceCd && item.seq == params.seq
+        );
+        return filtered;
+      }
+
+      if (params.serviceCd) {
+        const filtered = data.filter(
+          (item) => item.serviceCd == params.serviceCd
+        );
+        return filtered;
+      }
+
+      if (params.seq) {
+        const filtered = data.filter((item) => item.seq == params.seq);
+        return filtered;
+      }
+
+      return data;
+    },
+    [params]
+  );
 
   const query = useGetPushMsgListQuery(params, {
     select,
