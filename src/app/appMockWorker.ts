be7bce79fb761b900abbb262859dd04fc577eb62
SIULEE <e4.siu.lee@gmail.com>
2025-10-20 14:18:19 +0900

    테스트용 주석만 적용

--- src/app/appMockWorker.ts
+++ src/app/appMockWorker.ts
@@ -4,4 +4,5 @@ import { apiClientMocker } from "@/shared/api/netInstances";
 //NOTE - 여기에 등록하면 api 보다 우선시 되니 api 만들어지면 해당 mock handler 지울것!
 export default () => {
   menuApiMockHandler(apiClientMocker);
+  // pushMsgMngApiMockHandler(apiClientMocker);
 };
